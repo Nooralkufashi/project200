37514d03edf54833d2b9284fbd2bf912caea79a1
hayde <hayde@Hayder.attlocal.net>
2025-04-22 19:33:49 -0500

    added logic

--- src/project200/Demi1.java
+++ src/project200/Demi1.java
@@ -1,5 +1,13 @@
 package project200;
 
 public class Demi1 {
+	public static void main(String [] args) {
+		System.out.println("Java");
+		int a = 10;
+		int b = 20;
+		int result = a+b;
+		System.out.println(result);
+		
+	}
 
 }
